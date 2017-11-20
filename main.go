b90d57e793ec2c66d5a13da9dff5fae392f12332
CrazyMax <crazy-max@users.noreply.github.com>
2017-11-20 15:08:34 +0100

    Manage registry keys

--- main.go
+++ main.go
@@ -1,30 +1,55 @@
 package portableapps
 
 import (
+	"bytes"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
+	"syscall"
+	"time"
 
 	"github.com/google/logger"
 )
 
-type app struct {
-	ID           string
-	Name         string
-	Path         string
-	RootDataPath string
-	MainPath     string
-	DataPath     string
-	Process      string
-	Args         []string
-	WorkingDir   string
+type papp struct {
+	ID         string
+	Name       string
+	Path       string
+	AppPath    string
+	DataPath   string
+	Process    string
+	Args       []string
+	WorkingDir string
+}
+
+// CmdOptions options of command
+type CmdOptions struct {
+	Command    string
+	Args       []string
+	WorkingDir string
+	HideWindow bool
+}
+
+// CmdResult result of command
+type CmdResult struct {
+	Options  CmdOptions
+	ExitCode uint32
+	Stdout   string
+	Stderr   string
+}
+
+type RegExportImport struct {
+	Key  string
+	Arch string
+	File string
 }
 
 var (
-	// App main struct
-	App app
+	// Papp settings
+	Papp papp
 
 	// Log is the logger used by portapps
 	Log *logger.Logger
@@ -37,44 +62,37 @@ var (
 func Init() {
 	var err error
 
-	App.Path, err = filepath.Abs(filepath.Dir(os.Args[0]))
+	Papp.Path, err = filepath.Abs(filepath.Dir(os.Args[0]))
 	if err != nil {
 		Log.Fatal("Current path:", err)
 	}
 
-	App.MainPath = App.Path
-	App.RootDataPath = RootPathJoin("data")
-	App.DataPath = App.RootDataPath
+	Papp.DataPath = AppPathJoin("data")
 
-	Logfile, err = os.OpenFile(PathJoin(App.Path, App.ID+".log"), os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	Logfile, err = os.OpenFile(PathJoin(Papp.Path, Papp.ID+".log"), os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
 		Log.Fatal("Log file:", err)
 	}
 
-	Log = logger.Init(App.Name, false, false, Logfile)
+	Log = logger.Init(Papp.Name, false, false, Logfile)
 	Log.Info("--------")
-	Log.Infof("Starting %s...", App.Name)
-	Log.Infof("Current path: %s", App.Path)
+	Log.Infof("Starting %s...", Papp.Name)
+	Log.Infof("Current path: %s", Papp.Path)
 }
 
-// FindElectronMainFolder retrieved the app electron folder based on its prefix
-func FindElectronMainFolder(prefix string) string {
-	var electronMainPath string
-	Log.Infof("Lookup app folder in: %s", App.Path)
-	rootFiles, _ := ioutil.ReadDir(App.Path)
+// FindElectronAppFolder retrieved the app electron folder
+func FindElectronAppFolder(prefix string, source string) string {
+	Log.Infof("Lookup app folder in: %s", source)
+	rootFiles, _ := ioutil.ReadDir(source)
 	for _, f := range rootFiles {
 		if strings.HasPrefix(f.Name(), prefix) && f.IsDir() {
-			Log.Infof("Main folder found: %s", f.Name())
-			electronMainPath = PathJoin(App.Path, f.Name())
-			break
+			Log.Infof("Electron app folder found: %s", f.Name())
+			return f.Name()
 		}
 	}
-	if _, err := os.Stat(electronMainPath); err != nil {
-		Log.Fatalf("Electron main path does not exist with prefix '%s'", prefix)
-	}
 
-	Log.Infof("Electron main path found: %s", electronMainPath)
-	return electronMainPath
+	Log.Fatalf("Electron main path does not exist with prefix '%s' in %s", prefix, source)
+	return ""
 }
 
 // OverrideEnv to override an env var
@@ -84,26 +102,75 @@ func OverrideEnv(key string, value string) {
 	}
 }
 
-// Launch to execute the app
-func Launch() {
-	Log.Infof("Process: %s", App.Process)
-	Log.Infof("Args: %s", strings.Join(App.Args, " "))
-	Log.Infof("Working dir: %s", App.WorkingDir)
-	Log.Infof("Data path: %s", App.DataPath)
+// ExportRegKey export a registry key
+func ExportRegKey(reg RegExportImport) {
+	cmdResult, err := ExecCmd(CmdOptions{
+		Command:    "reg",
+		Args:       []string{"export", reg.Key, reg.File, "/y", fmt.Sprintf("/reg:%s", reg.Arch)},
+		HideWindow: true,
+	})
+	if err != nil {
+		Log.Fatalf("Cannot export registry key '%s': %v", reg.Key, err)
+	}
+	if cmdResult.ExitCode != 0 {
+		Log.Errorf(fmt.Sprintf("%d", cmdResult.ExitCode))
+		if len(cmdResult.Stderr) > 0 {
+			Log.Errorf(fmt.Sprintf("%s\n", cmdResult.Stderr))
+		}
+	}
+}
 
-	Log.Infof("Launch %s...", App.Name)
-	execApp := exec.Command(App.Process, App.Args...)
-	execApp.Dir = App.WorkingDir
+// ImportRegKey import a registry key
+func ImportRegKey(reg RegExportImport) {
+	// Save current reg key
+	ExportRegKey(RegExportImport{
+		Key:  reg.Key,
+		Arch: reg.Arch,
+		File: fmt.Sprintf("%s.%s", reg.File, time.Now().Format("20060102150405")),
+	})
 
-	defer Logfile.Close()
-	execApp.Stdout = Logfile
-	execApp.Stderr = Logfile
-
-	if err := execApp.Start(); err != nil {
-		Log.Fatalf("Cmd Start: %v", err)
+	// Check if reg file exists
+	if _, err := os.Stat(reg.File); err != nil {
+		return
 	}
 
-	execApp.Wait()
+	// Import
+	cmdResult, err := ExecCmd(CmdOptions{
+		Command:    "reg",
+		Args:       []string{"import", reg.File, fmt.Sprintf("/reg:%s", reg.Arch)},
+		HideWindow: true,
+	})
+	if err != nil {
+		Log.Fatalf("Cannot import registry file '%s': %v", reg.File, err)
+	}
+	if cmdResult.ExitCode != 0 {
+		Log.Errorf(fmt.Sprintf("%d", cmdResult.ExitCode))
+		if len(cmdResult.Stderr) > 0 {
+			Log.Errorf(fmt.Sprintf("%s\n", cmdResult.Stderr))
+		}
+	}
+}
+
+// Launch to execute the app
+func Launch() {
+	Log.Infof("Process: %s", Papp.Process)
+	Log.Infof("Args: %s", strings.Join(Papp.Args, " "))
+	Log.Infof("Working dir: %s", Papp.WorkingDir)
+	Log.Infof("Data path: %s", Papp.DataPath)
+
+	Log.Infof("Launch %s...", Papp.Name)
+	execute := exec.Command(Papp.Process, Papp.Args...)
+	execute.Dir = Papp.WorkingDir
+
+	execute.Stdout = Logfile
+	execute.Stderr = Logfile
+
+	Log.Infof("Exec %s %s", Papp.Process, strings.Join(Papp.Args, " "))
+	if err := execute.Start(); err != nil {
+		Log.Fatalf("Command failed: %v", err)
+	}
+
+	execute.Wait()
 }
 
 // CreateFolder to create a folder and get its path
@@ -125,7 +192,37 @@ func PathJoin(elem ...string) string {
 	return ""
 }
 
-// RootPathJoin to join paths from App.Path
-func RootPathJoin(elem ...string) string {
-	return PathJoin(append([]string{App.Path}, elem...)...)
+// AppPathJoin to join paths from Papp.Path
+func AppPathJoin(elem ...string) string {
+	return PathJoin(append([]string{Papp.Path}, elem...)...)
+}
+
+// ExecCmd to execute a system command
+func ExecCmd(options CmdOptions) (CmdResult, error) {
+	result := CmdResult{Options: options}
+
+	command := exec.Command(options.Command, options.Args...)
+	commandStdout := &bytes.Buffer{}
+	command.Stdout = commandStdout
+	commandStderr := &bytes.Buffer{}
+	command.Stderr = commandStderr
+	command.SysProcAttr = &syscall.SysProcAttr{HideWindow: options.HideWindow}
+
+	if options.WorkingDir != "" {
+		command.Dir = options.WorkingDir
+	}
+
+	Log.Infof("Exec %s %s", options.Command, strings.Join(options.Args, " "))
+	if err := command.Start(); err != nil {
+		return result, err
+	}
+
+	command.Wait()
+	waitStatus := command.ProcessState.Sys().(syscall.WaitStatus)
+
+	result.ExitCode = waitStatus.ExitCode
+	result.Stdout = strings.TrimSpace(commandStdout.String())
+	result.Stderr = strings.TrimSpace(commandStderr.String())
+
+	return result, nil
 }
