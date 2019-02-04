c0f993794da09998b27069c144069a1b0dc9967c
CrazyMax <crazy-max@users.noreply.github.com>
2019-02-04 18:11:06 +0100

    Add prepare step

--- .travis/install-deps.sh
+++ .travis/install-deps.sh
@@ -1,34 +1,26 @@
 #!/bin/bash
 set -e
 
-export GOPATH=${GOPATH:-/c/portapps_opt/gopath}
-export GOROOT=${GOROOT:-/c/portapps_opt/go}
-export JAVA_HOME=${JAVA_HOME:-/c/portapps_opt/jdk}
-export NODEJS_HOME=${NODEJS_HOME:-/c/portapps_opt/nodejs}
-
-export GOVERSION=${GOVERSION:-1.11.5}
-export NODEJS_VERSION=${NODEJS_VERSION:-11.9.0}
-
-# init
+echo "#### Init"
 rm -rf /c/portapps_opt
 mkdir -p /c/portapps_opt
 export PATH=${JAVA_HOME}/bin:${GOPATH}/bin:${GOPATH}/bin/windows_386:${GOROOT}/bin:${NODEJS_HOME}:${PATH}
 
-# jdk / ant
+echo "#### Installing Java JDK and Apache ANT"
 choco install jdk8 -params 'installdir=c:\\portapps_opt\\jdk' -y
 choco install ant --ignore-dependencies -y
 
-# nodejs
+echo "#### Installing Node.js ${NODEJS_VERSION}"
 curl -fsS -o node-v${NODEJS_VERSION}-win-x64.7z https://nodejs.org/dist/v${NODEJS_VERSION}/node-v${NODEJS_VERSION}-win-x64.7z
 7z x node-v${NODEJS_VERSION}-win-x64.7z -y -o/c/portapps_opt/
 mv /c/portapps_opt/node-v${NODEJS_VERSION}-win-x64 ${NODEJS_HOME}
 npm config set prefix "c:\\portapps_opt\\nodejs"
 
-# go
+echo "#### Installing Go ${GOVERSION}"
 curl -fsS -o go${GOVERSION}.windows-amd64.zip https://storage.googleapis.com/golang/go${GOVERSION}.windows-amd64.zip
 7z x go${GOVERSION}.windows-amd64.zip -y -o/c/portapps_opt/
 
-# check
+echo "#### Finalize"
 java -version
 ant -version
 go version
---
+++ .travis/prepare.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+export GOPATH=${GOPATH:-/c/portapps_opt/gopath}
+export GOROOT=${GOROOT:-/c/portapps_opt/go}
+export JAVA_HOME=${JAVA_HOME:-/c/portapps_opt/jdk}
+export NODEJS_HOME=${NODEJS_HOME:-/c/portapps_opt/nodejs}
+export GOVERSION=${GOVERSION:-1.11.5}
+export NODEJS_VERSION=${NODEJS_VERSION:-11.9.0}
