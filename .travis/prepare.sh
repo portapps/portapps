b1f3c3745467c9ae39ed37290ef7d744931354e9
CrazyMax <crazy-max@users.noreply.github.com>
2019-04-15 21:10:51 +0200

    Update libs

--- .travis/prepare.sh
+++ .travis/prepare.sh
@@ -5,8 +5,8 @@ export GOPATH=${GOPATH:-/c/portapps_opt/gopath}
 export GOROOT=${GOROOT:-/c/portapps_opt/go}
 export JAVA_HOME=${JAVA_HOME:-/c/portapps_opt/jdk}
 export NODEJS_HOME=${NODEJS_HOME:-/c/portapps_opt/nodejs}
-export GOVERSION=${GOVERSION:-1.11.5}
-export NODEJS_VERSION=${NODEJS_VERSION:-11.9.0}
+export GOVERSION=${GOVERSION:-1.12.4}
+export NODEJS_VERSION=${NODEJS_VERSION:-11.14.0}
 export PATH=${JAVA_HOME}/bin:${GOPATH}/bin:${GOPATH}/bin/windows_386:${GOROOT}/bin:${NODEJS_HOME}:${PATH}
 
 rm -rf /c/portapps_opt
