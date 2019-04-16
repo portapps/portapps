29570e91a486d12b2b4ded88eeb36eac74137a3b
CrazyMax <crazy-max@users.noreply.github.com>
2019-04-17 01:42:57 +0200

    Fix java dep

--- .travis/java.sh
+++ .travis/java.sh
@@ -2,7 +2,7 @@
 set -e
 
 #choco install jdk8 -params 'installdir=c:\\portapps_opt\\jdk' -y
-curl -fsS -o jdk-${JAVA_VERSION}-windows-i586.zip https://dl.bintray.com/crazy/tools/jdk-${JAVA_VERSION}-windows-i586.zip
+wget -q https://dl.bintray.com/crazy/tools/jdk-${JAVA_VERSION}-windows-i586.zip
 7z x jdk-${JAVA_VERSION}-windows-i586.zip -y -o/c/portapps_opt/
 
 java -version
