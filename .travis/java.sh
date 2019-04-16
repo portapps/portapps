7b61f9f884de387dbf16d1b3577b29f6beb1effa
CrazyMax <crazy-max@users.noreply.github.com>
2019-04-17 01:22:06 +0200

    Update jdk

--- .travis/java.sh
+++ .travis/java.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
-choco install jdk8 -params 'installdir=c:\\portapps_opt\\jdk' -y
+curl -fsS -o jdk-${JAVA_VERSION}-windows-i586.zip https://dl.bintray.com/crazy/tools/jdk-${JAVA_VERSION}-windows-i586.zip
+7z x jdk-${JAVA_VERSION}-windows-i586.zip -y -o/c/portapps_opt/
 
 java -version
--- .travis/prepare.sh
+++ .travis/prepare.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -e
 
+export JAVA_VERSION=${JAVA_VERSION:-8u211}
+export GOVERSION=${GOVERSION:-1.12.4}
+export NODEJS_VERSION=${NODEJS_VERSION:-11.14.0}
+
 export GOPATH=${GOPATH:-/c/portapps_opt/gopath}
 export GOROOT=${GOROOT:-/c/portapps_opt/go}
 export JAVA_HOME=${JAVA_HOME:-/c/portapps_opt/jdk}
 export NODEJS_HOME=${NODEJS_HOME:-/c/portapps_opt/nodejs}
-export GOVERSION=${GOVERSION:-1.12.4}
-export NODEJS_VERSION=${NODEJS_VERSION:-11.14.0}
+
 export PATH=${JAVA_HOME}/bin:${GOPATH}/bin:${GOPATH}/bin/windows_386:${GOROOT}/bin:${NODEJS_HOME}:${PATH}
 
 rm -rf /c/portapps_opt
