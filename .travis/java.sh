a08ab4da4e8909fbfb3e26fcbba0f5529e38cfec
CrazyMax <crazy-max@users.noreply.github.com>
2019-04-18 10:14:31 +0200

    Switch to OpenJDK 11.0.2

--- .travis/java.sh
+++ .travis/java.sh
@@ -4,18 +4,19 @@ set -e
 export TOOLS_PATH=${TOOLS_PATH:-/c/portapps_opt}
 
 export JAVA_HOME=${JAVA_HOME:-$TOOLS_PATH/jdk}
-export JAVA_VERSION=${JAVA_VERSION:-8u211}
+export JAVA_VERSION=${JAVA_VERSION:-11.0.2}
 
 cd ${TOOLS_PATH}
 
 if [ ! -f "${TOOLS_PATH}/jdk-${JAVA_VERSION}-windows-i586.zip" ]; then
-  echo "Downloading https://dl.bintray.com/crazy/tools/jdk-${JAVA_VERSION}-windows-i586.zip..."
-  wget -q https://dl.bintray.com/crazy/tools/jdk-${JAVA_VERSION}-windows-i586.zip
+  echo "Downloading https://download.java.net/java/GA/jdk11/9/GPL/openjdk-${JAVA_VERSION}_windows-x64_bin.zip..."
+  curl -fsS -o openjdk-${JAVA_VERSION}_windows-x64_bin.zip https://download.java.net/java/GA/jdk11/9/GPL/openjdk-${JAVA_VERSION}_windows-x64_bin.zip
   rm -rf ${JAVA_HOME}
 fi
 if [ ! -d "${JAVA_HOME}" ]; then
   echo "Extracting jdk-${JAVA_VERSION}-windows-i586.zip..."
-  7z x jdk-${JAVA_VERSION}-windows-i586.zip -y -o${TOOLS_PATH}/
+  7z x openjdk-${JAVA_VERSION}_windows-x64_bin.zip -y -o${TOOLS_PATH}/
+  mv ${TOOLS_PATH}/jdk-${JAVA_VERSION} ${JAVA_HOME}
 fi
 
 java -version
