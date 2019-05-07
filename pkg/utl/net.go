98ad394950c26adf6fad9828eea2e55c85b7305f
CrazyMax <crazy-max@users.noreply.github.com>
2019-05-07 19:01:59 +0200

    Add DownloadFile function

---
+++ pkg/utl/net.go
@@ -0,0 +1,26 @@
+package utl
+
+import (
+	"io"
+	"net/http"
+	"os"
+)
+
+// DownloadFile will download a url to a local file
+func DownloadFile(filepath string, url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	return err
+}
