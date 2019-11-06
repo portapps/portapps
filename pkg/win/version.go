2af05dcb10ae5a4b079f3d2c37264a72803fd694
CrazyMax <crazy-max@users.noreply.github.com>
2019-11-06 15:48:06 +0100

    Fix win.GetVersion function

--- pkg/win/version.go
+++ pkg/win/version.go
@@ -2,27 +2,30 @@ package win
 
 import (
 	"syscall"
-	"unsafe"
 )
 
-// Version returns Windows OS version
-// TODO: Replace with `windows.GetVersion()` when this is resolved: https://github.com/golang/go/issues/17835
-func Version() (major, minor, build uint32) {
-	var verStruct struct {
-		dwOSVersionInfoSize uint32
-		dwMajorVersion      uint32
-		dwMinorVersion      uint32
-		dwBuildNumber       uint32
-		dwPlatformId        uint32
-		szCSDVersion        [128]byte
+// Version identifies a Windows version by major, minor, and build number.
+type Version struct {
+	Major int
+	Minor int
+	Build int
+}
+
+// GetVersion returns the Windows version information. Applications not
+// manifested for Windows 8.1 or Windows 10 will return the Windows 8 OS version
+// value (6.2).
+// For a table of version numbers see:
+// https://msdn.microsoft.com/en-us/library/windows/desktop/ms724833(v=vs.85).aspx
+func GetVersion() (Version, error) {
+	// https://msdn.microsoft.com/en-us/library/windows/desktop/ms724439(v=vs.85).aspx
+	ver, err := syscall.GetVersion()
+	if err != nil {
+		return Version{}, err
 	}
 
-	ntoskrnl := syscall.MustLoadDLL("ntoskrnl.exe")
-	defer ntoskrnl.Release()
-
-	proc := ntoskrnl.MustFindProc("RtlGetVersion")
-	verStruct.dwOSVersionInfoSize = uint32(unsafe.Sizeof(verStruct))
-	proc.Call(uintptr(unsafe.Pointer(&verStruct)))
-
-	return verStruct.dwMajorVersion, verStruct.dwMinorVersion, verStruct.dwBuildNumber
+	return Version{
+		Major: int(ver & 0xFF),
+		Minor: int(ver >> 8 & 0xFF),
+		Build: int(ver >> 16),
+	}, nil
 }
